eb58a286ad747b71482d5999bdb0c8279c819f72
vir-ren <tingan.ren@gmail.com>
2025-09-21 22:58:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
